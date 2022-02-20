f465e02449451118b83e75aaed7a404eba79974a
mepo <mulder.amj@gmail.com>
2022-02-20 15:34:32 +0100

    main now calls the fetchProgram routine without having to read an rfid

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ const MachineID = "MPS_System_403_1_DiK_01"
 const OPCHUB = new OPCHub(ServiceID, MachineID);
 
 const Distributing = await OPCHUB.addDevice("Distributing", "opc.tcp://192.168.1.10:4840")
-// const Joining = await OPCHUB.addDevice("Joining", "opc.tcp://192.168.1.30:4840")
-// const Sorting = await OPCHUB.addDevice("Sorting", "opc.tcp://192.168.1.40:4840")
+const Joining = await OPCHUB.addDevice("Joining", "opc.tcp://192.168.1.30:4840")
+const Sorting = await OPCHUB.addDevice("Sorting", "opc.tcp://192.168.1.40:4840")
 
 
 const webserverFile = "./Web Server/server.js"
@@ -26,6 +26,7 @@ const options = {
 const server = fork(webserverFile);
 const serviceExecutor = new ServiceExecutor(ServiceID, OPCHUB, server);
 
-// uploadProgram(ServiceID, 118, "./service resources/dance_for_115.js");
+// await uploadProgram(ServiceID, 1, "./service resources/1.js");
 
 // await OPCHUB.devices["Distributing"]["client"].installSubscription('ns=3;s="FB_Zeitkontrolle_Adriaan_scl_DB"."CoId"', serviceExecutor.fetchCallback())
+await serviceExecutor.fetchProgram("1");
